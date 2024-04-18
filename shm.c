a7424bfc57ec57d368c121e5fab039f71ba37f07
SREEKUMAR S (23008070) <151705853+guru14789@users.noreply.github.com>
2024-04-18 09:06:07 +0530

    Update shm.c

--- shm.c
+++ shm.c
@@ -1,1 +1,21 @@
-/
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main()
+{
+	key_t key = ftok("shmfile", 65);
+
+	int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+      printf("Shared memory id = %d \n",shmid);
+	char* str = (char*)shmat(shmid, (void*)0, 0);
+	
+    printf("Write Data : ");
+	fgets(str, 1024, stdin);
+
+	printf("Data written in memory: %s\n", str);
+
+	shmdt(str);
+
+	return 0;
+}
